309a16132b0f3f36538ffeeadf4db01e1d50cc0a
Rahul <rahul.wa47@gmail.com>
2026-01-10 17:03:28 +0530

    completed analyze function and it's test

--- src/analyze-array.js
+++ src/analyze-array.js
@@ -1,5 +1,21 @@
 
 
-export function analyzeArray () {
+export function analyzeArray (arr) {
+    const average = arr.reduce((acc, curr) => {
+        acc+=curr;
+        return acc
+    },0)/arr.length
     
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    const length = arr.length
+
+    return {
+        average,
+        min,
+        max,
+        length
+    }
 }
+
+console.log(analyzeArray([1,8,3,4,2,6]))
